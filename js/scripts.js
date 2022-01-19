34c2eab6440cf8c9fb9952b0fa88a37de1401760
S. Espinet <s.lou.espinet@gmail.com>
2022-01-19 09:30:47 -0800

    add bold passage UI logic
    Co-authored-by: Justin Dechow <justin.dechow@gmail.com>
    Co-authored-by: Liam Eller <liamthelastson@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,9 @@
+//Utility Logic
+
+function noInputtedWord(word, text) {
+  return ((text.trim().length === 0) || (word.trim().length === 0));
+}
+
 // Business Logic
 
 function wordCounter(text) {
@@ -14,8 +20,9 @@ function wordCounter(text) {
   return wordCount;
 }
 
+
 function numberOfOccurrencesInText(word, text) {
-  if ((text.trim().length === 0) || (word.trim().length === 0)){
+  if (noInputtedWord(word, text)) {
     return 0;
   }
   const wordArray = text.split(" ");
@@ -29,6 +36,9 @@ function numberOfOccurrencesInText(word, text) {
 }
 
 function boldPassage(word, text) {
+  if (noInputtedWord(word, text)) {
+    return "";
+  }
   let htmlString = "<p>";
   let textArray = text.split(" ");
   textArray.forEach(function(element, index) {
@@ -56,5 +66,7 @@ $(document).ready(function(){
     const occurrencesOfWord = numberOfOccurrencesInText(word, passage);
     $("#total-count").html(wordCount);
     $("#selected-count").html(occurrencesOfWord);
+    $("#bolded-passage").html(boldPassage(word, passage));
   });
 });
+
